9ca0d70deecb8fc9bb625568bf3494744fbb167e
PierluigiRebiscini <pierluigi.rebiscini@gmail.com>
2026-03-10 21:39:37 +0100

    aggiunta pagina "not found" in caso di path non corretto

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <section className="img-e404">
+            <img src="https://staticgeopop.akamaized.net/wp-content/uploads/sites/32/2024/10/errore-404-page-not-found-1200x675.jpg" alt="Error 404 Not Found" />
+        </section>
+    )
+
+}
